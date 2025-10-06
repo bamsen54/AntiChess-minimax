01b68f906a3379a44a015d7894ec4652c530f226
Simon Toivola <simontoivola@gmail.com>
2025-10-06 19:41:45 +0200

    make screenWidth and screenHeight member variables

--- src/main/java/com/AntiChess/Gui.java
+++ src/main/java/com/AntiChess/Gui.java
@@ -14,17 +14,19 @@ public class Gui {
     public static int squareSize;
     public static int marginAll;
 
+    public static int screenWidth;
+    public static int screenHeight;
+
     public static Color lightSquareColor = RayU.color( 241, 242, 249 );
     public static Color darkSquareColor  = RayU.color( 53,   53,  64 );
-
-    public static boolean isFlipped = false;
 
     public static HashMap<Character, Texture> icons = new HashMap<Character, Texture>();
 
     public static void init() {
 
-        final int screenWidth             = GetScreenWidth();
-        final int screenHeight            = GetScreenHeight();
+        screenWidth  = GetScreenWidth();
+        screenHeight = GetScreenHeight();
+
         final int smallestDimension = Math.min( screenWidth, screenHeight );
 
         marginAll = Math.max( (int) ( 0.025 * GetScreenWidth() ), (int) ( 0.025 * GetScreenHeight() ) ); // 2.5% of width
