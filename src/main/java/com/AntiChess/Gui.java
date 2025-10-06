01955cc9ab2d0f6d2c1902e20bec4a7c216b030f
Simon Toivola <simontoivola@gmail.com>
2025-10-06 19:40:08 +0200

    factorizing

--- src/main/java/com/AntiChess/Gui.java
+++ src/main/java/com/AntiChess/Gui.java
@@ -12,16 +12,25 @@ public class Gui {
     public static int boardPositionX;
     public static int boardPositionY;
     public static int squareSize;
+    public static int marginAll;
 
     public static Color lightSquareColor = RayU.color( 241, 242, 249 );
     public static Color darkSquareColor  = RayU.color( 53,   53,  64 );
 
-    public static int marinAll;
+    public static boolean isFlipped = false;
 
     public static HashMap<Character, Texture> icons = new HashMap<Character, Texture>();
 
     public static void init() {
 
+        final int screenWidth             = GetScreenWidth();
+        final int screenHeight            = GetScreenHeight();
+        final int smallestDimension = Math.min( screenWidth, screenHeight );
 
+        marginAll = Math.max( (int) ( 0.025 * GetScreenWidth() ), (int) ( 0.025 * GetScreenHeight() ) ); // 2.5% of width
+
+        squareSize     = (int) ( ( smallestDimension - 2 * marginAll ) / 8.0);
+        boardPositionY = marginAll;
+        boardPositionX = screenWidth / 2 - 4 * squareSize;
     }
 }
