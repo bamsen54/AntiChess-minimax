3bc86e2f2a2934060bc3aa8b09d1e2a67f047937
Simon Toivola <simontoivola@gmail.com>
2025-10-06 18:55:40 +0200

    information needed for an anti chess game

---
+++ src/main/java/com/antichess/Game.java
@@ -0,0 +1,28 @@
+package com.antichess;
+
+import java.util.ArrayList
+
+public class Game {
+
+    ublic char[][] board = {{'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+            {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+            {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+            {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}
+    };
+
+    public String turn    = "white";
+
+    // if en passant is possible, this is the en passant square.
+    // en passant square is the square that pawn "captures" but there is no piece there
+    public int[] en_passant_square = {};
+
+    public int half_move_clock  = 0;
+    public int full_move_number = 1;
+
+    // for five-fold repetition stores fen
+    ArrayList<String> history = new ArrayList<>();
+}
