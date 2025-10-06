487cd1f77356b060a301bca7890b090f6088eca7
Simon Toivola <simontoivola@gmail.com>
2025-10-06 18:31:59 +0200

    window created

--- src/main/java/com/antichess/Main.java
+++ src/main/java/com/antichess/Main.java
@@ -1,9 +1,13 @@
 package com.antichess;
 
+import static com.raylib.Raylib.*;
+
 public class Main {
 
     void main() {
 
+        InitWindow( (int) 1.2 * 1280, (int) 1.2 * 720, "Anti Chess" );
         
+        CloseWindow();
     }
 }
